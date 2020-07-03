f751e06bfce54b796a0ab5f064c03e4848f397e0
savalascolbert@gmail.com <brownie1>
2020-07-03 14:59:49 -0700

    initial commit

---
+++ hello_blocks/frontend/index.js
@@ -0,0 +1,9 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+
+function HelloWorldBlock() {
+    // YOUR CODE GOES HERE
+    return <div>Hello again, universe- Fly me to the moon 🚀</div>;
+}
+
+initializeBlock(() => <HelloWorldBlock />);
